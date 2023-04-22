e4b5e43038350d8511e9fe5d79357da8ce7663b8
Muhammad Annas Baig <83984612+Annas03@users.noreply.github.com>
2023-04-22 15:58:01 +0500

    Update main.py

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,3 +1,12 @@
+import math
+
+distances = {
+    '1': {'2': 10, '3': 15, '4': 20},
+    '2': {'1': 10, '3': 35, '4': 25},
+    '3': {'1': 15, '2': 35, '4': 30},
+    '4': {'1': 20, '2': 25, '3': 30}
+}
+
 solution = []
 
 def backtrack(perm, sol, level):
@@ -11,6 +20,7 @@ def backtrack(perm, sol, level):
             t_sol = []
             for m in range(len(sol)):
                 t_sol.append(sol[m])
+            t_sol.append(t_sol[0])
             solution.append(t_sol)
             sol.pop()
             sol.pop()
@@ -20,6 +30,18 @@ def backtrack(perm, sol, level):
     if(len(sol)!=0):
         sol.pop()
 
+def minCost(solution, distances):
+    min_cost = math.inf
+
+    for i in range(len(solution)):
+        cost = 0
+        for j in range(len(solution[i]) - 1):
+            cost += distances[str(solution[i][j])][str(solution[i][j+1])]
+        if cost < min_cost:
+            min_cost = cost
+    return min_cost
+
 
 backtrack([1,2,3,4], [], 0)
-print(solution)
+print(minCost(solution, distances))
+
