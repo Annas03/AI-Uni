c7effb6be69157321bce4accc223d2e2f63e677f
Muhammad Annas Baig <83984612+Annas03@users.noreply.github.com>
2023-05-07 22:23:33 +0500

    Update towerOfHanoi.py

--- Lab6/towerOfHanoi.py
+++ Lab6/towerOfHanoi.py
@@ -1,56 +1,38 @@
 
 def arrange(stackA, stackB, stackC):
+
     while True:
-        if(len(stackA) == 0):
-            if(len(stackB) == 0):
-                return stackC
-            elif(len(stackC) == 0):
-                return stackB
-            elif (stackB[len(stackB)-1] > stackC[len(stackC)-1]):
-                temp1 = stackC[:]
-                temp2 = stackB[:]
-                stackB = arrange(temp1, temp2, [])
-                stackC = []
-            else:
-                temp1 = stackB[:]
-                temp2 = stackC[:]
-                stackC = arrange(temp1, temp2, [])
-                stackB = []
-            continue
-            
-        if(len(stackB) == 0 or stackB[0] - stackA[0] == 1):
+
+        if(len(stackB) == 0 or (len(stackA)!=0 and stackB[0] - stackA[0] == 1)):
             stackB.insert(0, stackA[0])
             stackA.pop(0)
             continue
-        if((len(stackC) == 0 or stackA[0] < stackC[0]) and len(stackA) != 0):
+        if(len(stackA) != 0 and (len(stackC) == 0 or stackA[0] < stackC[0])):
             stackC.insert(0, stackA[0])
             stackA.pop(0)
 
         if(len(stackC) != 0 and len(stackB) != 0):
             if(len(stackA) != 0 and (stackA[0] - stackB[0] < 0)):
-                temp1 = stackA[:]
-                temp2 = stackB[:]
-                stackB = arrange(temp1, temp2, [])
+                stackB = arrange(stackA[:], stackB[:], [])
                 stackA = stackC
                 stackC = []
             elif(len(stackA) != 0 and (stackA[0] - stackC[0] < 0)):
-                temp1 = stackC[:]
-                temp2 = stackA[:]
-                stackA = arrange(temp1, temp2, [])
+                stackA = arrange(stackC[:], stackA[:], [])
                 stackC = []
             elif (stackB[len(stackB)-1] > stackC[len(stackC)-1]):
-                temp1 = stackC[:]
-                temp2 = stackB[:]
-                stackB = arrange(temp1, temp2, [])
+                stackB = arrange(stackC[:], stackB[:], [])
                 stackC = []
             else:
-                temp1 = stackB[:]
-                temp2 = stackC[:]
-                stackC = arrange(temp1, temp2, [])
+                stackC = arrange(stackB[:], stackC[:], [])
                 stackB = []
 
+        if(len(stackA) == 0):
+            if(len(stackB) == 0):
+                return stackC
+            elif(len(stackC) == 0):
+                return stackB
 
-stackA = [1,2,3,4]
+stackA = [1,2,3,4,5,6,7]
 stackB = []
 stackC = []
 print(arrange(stackA, stackB, stackC))
